bb433b8f985096f821fa7b40035c18ff026077a1
TrungDT <thanhtrung.it25@gmail.com>
2018-05-02 23:58:44 +0700

    Setup contentful for Blog stateful component

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,3 +1,41 @@
 import React from 'react';
-const Blog = () => <p>This is the Blog Page</p>;
+import * as contentful from 'contentful';
+
+class Blog extends React.Component {
+  state = {
+    posts: [],
+  };
+
+  client = contentful.createClient({
+    space: 'spcwng43cfp1',
+    accessToken:
+      '3c4a374b63c19c010b795fd05903cd282fac1f68c5383d04e6563dca17c4ed7d',
+  });
+
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+
+  fetchPosts = () => this.client.getEntries();
+
+  setPosts = response => {
+    console.log(response);
+    this.setState({
+      posts: response.items,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>This is the Blog Page</p>
+        <br />
+        {this.state.posts.map(({ fields }, i) => (
+          <pre key={i}>{JSON.stringify(fields, null, 2)}</pre>
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Blog;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as contentful from 'contentful';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
