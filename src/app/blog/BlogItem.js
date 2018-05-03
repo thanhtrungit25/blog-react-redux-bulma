e4bd61f6b7c884b36e2b22581904e8d3966ad506
TrungDT <thanhtrung.it25@gmail.com>
2018-05-03 10:21:24 +0700

    adding markdown

--- src/app/blog/BlogItem.js
+++ src/app/blog/BlogItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import * as MarkDown from 'react-markdown';
 
 const BlogItem = props => (
   <div className="box content">
     <h1>{props.title}</h1>
-    <p>{props.content}</p>
+    <MarkDown source={props.content} />
   </div>
 );
 
