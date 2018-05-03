8124eb953c46dd70bfd6e61e9228c9bd5afffc64
TrungDT <thanhtrung.it25@gmail.com>
2018-05-03 09:57:23 +0700

    styed blog page

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as contentful from 'contentful';
 import BlogItem from './blog/BlogItem';
+import PageHeader from './components/PageHeader';
 
 class Blog extends React.Component {
   state = {
@@ -29,7 +30,13 @@ class Blog extends React.Component {
   render() {
     return (
       <div>
-        <p>This is the Blog Page</p>
+        <PageHeader color="is-info" title="Code Blog">
+          Your standard <strong>JavaScript</strong> programming blog, albeit,
+          probably not very good, but I will at least try to keep it
+          entertaining. This blog is a chronological mix of random posts on
+          Angular, React, Functional Programming, and my{' '}
+          <strong>project walkthroughs</strong>.
+        </PageHeader>
         <br />
         {this.state.posts.map(({ fields }, i) => (
           <BlogItem key={i} {...fields} />
