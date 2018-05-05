21a51fdad6f6905597f21e74031d4498585e46ff
TrungDT <thanhtrung.it25@gmail.com>
2018-05-05 09:49:13 +0700

    Apply blog state to Blog component

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import * as contentful from 'contentful';
+import { connect } from 'react-redux';
 import BlogItem from './blog/BlogItem';
 import PageHeader from './components/PageHeader';
 
 class Blog extends React.Component {
-  state = {
-    posts: [],
-  };
-
-  client = contentful.createClient({
-    space: 'spcwng43cfp1',
-    accessToken:
-      '3c4a374b63c19c010b795fd05903cd282fac1f68c5383d04e6563dca17c4ed7d',
-  });
-
-  componentDidMount() {
-    this.fetchPosts().then(this.setPosts);
-  }
-
-  fetchPosts = () => this.client.getEntries();
-
-  setPosts = response => {
-    console.log(response);
-    this.setState({
-      posts: response.items,
-    });
-  };
-
   render() {
     return (
       <div>
@@ -38,7 +15,7 @@ class Blog extends React.Component {
           <strong>project walkthroughs</strong>.
         </PageHeader>
         <br />
-        {this.state.posts.map(({ fields }, i) => (
+        {this.props.blog.posts.map(({ fields }, i) => (
           <BlogItem key={i} {...fields} />
         ))}
       </div>
@@ -46,4 +23,10 @@ class Blog extends React.Component {
   }
 }
 
-export default Blog;
+function mapStateToProps(state, ownProps) {
+  return {
+    blog: state.blog,
+  };
+}
+
+export default connect(mapStateToProps)(Blog);
